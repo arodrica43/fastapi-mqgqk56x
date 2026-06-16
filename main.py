e23a3ae8ccbe0b9568bd4b457dabac3a927ef441
arodrica43 <lexws33@gmail.com>
2026-06-16 16:48:24 +0200

    connecting MongoDB - 1

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from typing import Optional
-
 from fastapi import FastAPI
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
+uri = "mongodb+srv://lexws33:KdkPwU2AZ4wU9jNL@cluster0.f0fjujx.mongodb.net/?appName=Cluster0"
 app = FastAPI()
-
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World!!"}
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        return {"Pinged your deployment. You successfully connected to MongoDB!"}
+    except Exception as e:
+        return {"Error": "Unable to connect to the database."}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
