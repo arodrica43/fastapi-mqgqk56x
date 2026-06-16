e2e0eeb83e6185806b0a4f6a7a8dbb10a2887388
arodrica43 <lexws33@gmail.com>
2026-06-16 17:40:20 +0200

    final commit - completed GET/POST

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from typing import Optional
 from fastapi import FastAPI
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+import json
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
 
 uri = "mongodb+srv://lexws33:KdkPwU2AZ4wU9jNL@cluster0.f0fjujx.mongodb.net/?appName=Cluster0"
 app = FastAPI()
@@ -19,13 +24,22 @@ async def root():
 
 @app.get("/users/{user_name}")
 async def get_user(user_name: str):
-    db = client["users"]
-    collection = db["Plain Users"]
+    db = client["user"]
+    collection = db["plain_user"]
+    print(collection)
     user = collection.find_one({"name": user_name})
     if user:
-        return {"user": user}
+        return {"user": json.loads(json.dumps(user, default=str))}
     else:
         return {"Error": "User not found."}
+    
+@app.post("/users")
+async def create_user(user: dict):
+    db = client["user"]
+    collection = db["plain_user"]
+    print(user)
+    result = collection.insert_one(json.loads(json.dumps(user, default=str)))
+    return {"Inserted ID": str(result.inserted_id)}
 
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
